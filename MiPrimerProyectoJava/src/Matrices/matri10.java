a67278f04e49e7e028c259d4a5f2621b1aafe79f
Dario Evelio Chacon Parada <0706dario@gmail.com>
2025-04-01 10:25:22 -0500

    ejercicio

--- MiPrimerProyectoJava/src/Matrices/matri10.java
+++ MiPrimerProyectoJava/src/Matrices/matri10.java
@@ -2,6 +2,12 @@ package Matrices;
 
 public class matri10 {
     public static void main(String[] args) {
-        
+        int[][] distancias = {
+            {0, 10, 15, 20},
+            {10, 0, 35, 25},
+            {15, 35, 0, 30},
+            {20, 25, 30, 0}
+        };
+        System.out.println("La distancia entre Ciudad 1 y Ciudad 3 es: " + distancias[0][2]);
     }
 }
